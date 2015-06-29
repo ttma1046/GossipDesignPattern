e1d697f823b6bce13789777ba060385441ebc04a
Chandler Fang <chandlerfang@gmail.com>
2015-06-29 21:34:10 +1000

    Project Initial

---
+++ GossipDesignPattern/StrategyPattern/Calc.v1.0.cs
@@ -0,0 +1,25 @@
+﻿namespace GossipDesignPattern.StrategyPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Calc
+    {
+        double total = 0.0d;
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            double totalPrices = Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtNum.Text);
+
+            total = total + totalPrices;
+
+            lbxList.Items.Add("Price: " + txtPrice.Text + " Quantity: " + txtNum.Text + " Total: " + totalPrices.ToString());
+
+            lblResult.Text = total.ToString();
+            
+        }
+    }
+}
